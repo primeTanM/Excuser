958d4374515d80317b8779ba68af247a4f2ff175
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-01-02 00:30:28 +0530

    generalised category route and added digit RE (\d+)

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
-var express = require('express');
-var excusesRepository = require('./excuseRepository');
+var express = require("express");
+var excusesRepository = require("./excuseRepository");
 var app = express();
 var port = process.env.PORT || 3000;
 
-
-
-app.all('*', function (req, res, next) {
-    res.set('Access-Control-Allow-Origin', '*');
-    next();
+app.all("*", function (req, res, next) {
+  res.set("Access-Control-Allow-Origin", "*");
+  next();
 });
 
-app.get('/', function (req, res) {
-    res.send('<h1>Excuse Generator</h1>');
+app.get("/", function (req, res) {
+  res.send("<h1>Excuse Generator</h1>");
 });
 
-app.get('/v1', function (req, res) {
-    res.send('<h1>This is ithe initial verison...</h1>');
+app.get("/v1", function (req, res) {
+  res.send("<h1>This is ithe initial verison...</h1>");
 });
 
 // returns the excuse having the specific id
-app.get('/v1/excuse/id/:num', function(req, res) {
-    res.send(excusesRepository.getByID(req.params.num));
+app.get("/v1/excuse/id/:num(\\d+)?", function (req, res) {
+  res.send(excusesRepository.getByID(req.params.num));
 });
 
 // returns random excuses
-app.get('/v1/excuse/:num?', function (req, res, next) {
-    res.send(excusesRepository.getRandom(req.params.num || 1));
+app.get("/v1/excuse/:num(\\d+)?", function (req, res, next) {
+    // res.send("hello");
+  res.send(excusesRepository.getRandom(req.params.num || 1));
 });
 
+app.get("/v1/excuse/:category", function (req, res, next) {
+    res.send(excusesRepository.getByCategory(req.params.category, 1));
+  });
 
-// returns family related excuses
-app.get('/v1/excuse/family/:num?', function (req, res, next) {
-    res.send(excusesRepository.getByCategory("family", req.params.num || 1));
-});
-
-// returns work related excuses
-app.get('/v1/excuse/office/:num?', function (req, res, next) {
-    res.send(excusesRepository.getByCategory("office", req.params.num || 1));
-});
-
-// returns children related excuses
-app.get('/v1/excuse/children/:num?', function (req, res, next) {
-    res.send(excusesRepository.getByCategory("children", req.params.num || 1));
+app.get("/v1/excuse/:category/:num(\\d+)?", function (req, res, next) {
+  res.send(excusesRepository.getByCategory(req.params.category, req.params.num || 1));
 });
 
 
 app.listen(port, function () {
-    console.log('Server running on port', port);
-})
+  console.log("Server running on port", port);
+});
