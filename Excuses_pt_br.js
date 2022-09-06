bfda4514b23c581c9dbd8f6288d7b2e21de5c208
Zoyo - Lucas Queiroz <lucasoliveiraoueiroz@gmail.com>
2022-09-05 23:00:55 -0300

    feat: add a optional param 'language' on header to change de excuses language
    related #9

---
+++ Excuses_pt_br.js
@@ -0,0 +1,465 @@
+module.exports = [
+    // ---------- Family ----------
+    {
+        id: 1,
+        excuse: "Meu amigo chegou atrasado no trabalho e eu tive que ir trabalhar no lugar dele.",
+        category: "family",
+    },
+    {
+        id: 2,
+        excuse: "Meu chefe estava em reunião e eu tive que assumir o lugar dele.",
+        category: "family",
+    },
+    {
+        id: 3,
+        excuse: "Tive que bater em uns caras.",
+        category: "family",
+    },
+    {
+        id: 4,
+        excuse: "Meu carro quebrou.",
+        category: "family",
+    },
+    {
+        id: 5,
+        excuse: "Não consegui encontrar minhas chaves.",
+        category: "family",
+    },
+    {
+        id: 6,
+        excuse: "Estou com muita dor na barriga.",
+        category: "family",
+    },
+    {
+        id: 7,
+        excuse: "Meu amigo se acidentou e eu tive que ir trabalhar no lugar dele.",
+        category: "family",
+    },
+    {
+        id: 8,
+        excuse: "Meu chefe acabou de me ligar dizendo que preciso pegar um voo para uma reunião emergêncial.",
+        category: "family",
+    },
+    {
+        id: 9,
+        excuse: "Isso foi hoje? Puts, prometi pra minha esposa que teríamos um encontro esta noite.",
+        category: "family",
+    },
+    {
+        id: 10,
+        excuse: "A mãe de um colega acabou de falecer.",
+        category: "family",
+    },
+    {
+        id: 11,
+        excuse: "Tenho que trabalhar amanhã bem cedo.",
+        category: "family",
+    },
+    {
+        id: 12,
+        excuse: "Sendo sincero, eu prefiro ficar em casa lendo.",
+        category: "family",
+    },
+    {
+        id: 13,
+        excuse: "PT-BR | Don’t bet on seeing me at the party later. I’ve been throwing up all morning.",
+        category: "family",
+    },
+    {
+        id: 14,
+        excuse: "PT-BR | I was about to drive out, but I realized that I have a flat tire.",
+        category: "family",
+    },
+    {
+        id: 15,
+        excuse: "PT-BR | My baby sitter canceled on me at the very last minute",
+        category: "family",
+    },
+    {
+        id: 16,
+        excuse: "PT-BR | It’s raining cats and dogs out here. I don’t think it’s a good idea to drive in this weather.",
+        category: "family",
+    },
+    {
+        id: 17,
+        excuse: "PT-BR | I have exams starting tomorrow and I have to devote my time to studying",
+        category: "family",
+    },
+    {
+        id: 18,
+        excuse: "PT-BR | My cab firm said there won’t be any availability for an hour. ",
+        category: "family",
+    },
+    {
+        id: 19,
+        excuse: "PT-BR | I would’ve loved to have gone out tonight but I’m currently looking for my wallet.",
+        category: "family",
+    },
+    {
+        id: 20,
+        excuse: "PT-BR | There has been a drainage issue at my home and I am waiting for my plumber",
+        category: "family",
+    },
+
+    // ---------- Office ----------
+
+    {
+        id: 101,
+        excuse: "PT-BR | My wife and son were sick and I have to take care of them.",
+        category: "office",
+    },
+    {
+        id: 102,
+        excuse: "PT-BR | I had to take care of my dog because he is sick.",
+        category: "office",
+    },
+    {
+        id: 103,
+        excuse: "PT-BR | My mother-in-law passed away.",
+        category: "office",
+    },
+    {
+        id: 104,
+        excuse: "PT-BR | My wife had to go to the hospital for delivery.",
+        category: "office",
+    },
+    {
+        id: 105,
+        excuse: "PT-BR | My son had to go to the hospital, he had a bone fracture.",
+        category: "office",
+    },
+    {
+        id: 106,
+        excuse: "PT-BR | My wife had to go to the hospital for a broken arm.",
+        category: "office",
+    },
+    {
+        id: 107,
+        excuse: "PT-BR | My son had to go to the hospital for a broken leg.",
+        category: "office",
+    },
+    {
+        id: 108,
+        excuse: "PT-BR | My parents showed up unannounced at my house.",
+        category: "office",
+    },
+    {
+        id: 109,
+        excuse: "PT-BR | I got involved in an accident.",
+        category: "office",
+    },
+    {
+        id: 110,
+        excuse: "PT-BR | Traffic stopped because of a broken truck.",
+        category: "office",
+    },
+    {
+        id: 111,
+        excuse: "PT-BR | I have a dentist appointment and I cannot reschedule it.",
+        category: "office",
+    },
+    {
+        id: 112,
+        excuse: "PT-BR | A major delivery is expected today and I need to be in-person to receive it.",
+        category: "office",
+    },
+    {
+        id: 113,
+        excuse: "PT-BR | I had a family function last night and I am really tired.",
+        category: "office",
+    },
+    {
+        id: 114,
+        excuse: "PT-BR | My specs broke and I cannot see a thing.",
+        category: "office",
+    },
+    {
+        id: 115,
+        excuse: "PT-BR | I have come down with a flu due to season change.",
+        category: "office",
+    },
+    {
+        id: 116,
+        excuse: "PT-BR | I have to attend Parents Teacher meeting for my kids' school",
+        category: "office",
+    },
+    {
+        id: 117,
+        excuse: "PT-BR | I've had red eyes since I woke up and I am facing difficulty looking at screens",
+        category: "office",
+    },
+    {
+        id: 118,
+        excuse: "PT-BR | The last bus from my house left early and I have no transportation.",
+        category: "office",
+    },
+    {
+        id: 119,
+        excuse: "PT-BR | I have got work at the bank",
+        category: "office",
+    },
+    {
+        id: 120,
+        excuse: "PT-BR | A crime took place in my locality. And police has asked everyone to not leave the premises",
+        category: "office",
+    },
+    {
+        id: 121,
+        excuse: "PT-BR | I have a violent cough and I don't want to share it with the rest of my colleagues today.",
+        category: "office",
+    },
+    {
+        id: 122,
+        excuse: "PT-BR | My car's battery has unfortunately died and I am stranded at home.",
+        category: "office",
+    },
+    {
+        id: 123,
+        excuse: "PT-BR | I'm feeling burnt out and I need a mental health day.",
+        category: "office",
+    },
+    {
+        id: 124,
+        excuse: "PT-BR | I promised my aunts that I would meet them this reunion.",
+        category: "office",
+    },
+    {
+        id: 125,
+        excuse: "PT-BR | My child is having their final dance performance at school and I promised her that I would be there.",
+        category: "office",
+    },
+    {
+        id: 126,
+        excuse: "PT-BR | The water system at my house is down and I am trying to fix it.",
+        category: "office",
+    },
+    {
+        id: 127,
+        excuse: "PT-BR | I ate at this seafood place the other day and now my stomach can't stop hurting.",
+        category: "office",
+    },
+    {
+        id: 128,
+        excuse: "PT-BR | My neck has been stuck in a funny position all morning can I cannot look at the screen properly.",
+        category: "office",
+    },
+    {
+        id: 129,
+        excuse: "PT-BR | My rabbit just gave birth and I need to make arrangements for 7 more rabbits now.",
+        category: "office",
+    },
+    {
+        id: 130,
+        excuse: "PT-BR | I have a really, really bad headache.",
+        category: "office",
+    },
+
+    // ---------- Children ----------
+
+    {
+        id: 201,
+        excuse: "PT-BR | I have a really important work assignment.",
+        category: "children",
+    },
+    {
+        id: 202,
+        excuse: "PT-BR | My boss was really in a bad mood today.",
+        category: "children",
+    },
+    {
+        id: 203,
+        excuse: "PT-BR | My boss is going to fire me if I don't finish my task.",
+        category: "children",
+    },
+    {
+        id: 204,
+        excuse: "PT-BR | Mom just called me saying that I need to get to supermarket for a shopping trip.",
+        category: "children",
+    },
+    {
+        id: 205,
+        excuse: "PT-BR | My best friend fell down to the stairs so I had to take him to the hospital.",
+        category: "children",
+    },
+    {
+        id: 206,
+        excuse: "PT-BR | The car has been out of gas since yesterday.",
+        category: "children",
+    },
+    {
+        id: 207,
+        excuse: "PT-BR | My temperature is really high today.",
+        category: "children",
+    },
+    {
+        id: 208,
+        excuse: "PT-BR | Mom is very unwell and I have to take her to the hospital.",
+        category: "children",
+    },
+    {
+        id: 209,
+        excuse: "PT-BR | Police are on their way to arrest me so I have to escape.",
+        category: "children",
+    },
+    {
+        id: 210,
+        excuse: "PT-BR | Doctor told me that I have a big worm in my stomach.",
+        category: "children",
+    },
+
+    // ---------- College ----------
+
+    {
+        id: 301,
+        excuse: "PT-BR | I forgot my driving license and got in a heated argument with traffic police.",
+        category: "college",
+    },
+    {
+        id: 302,
+        excuse: "PT-BR | As my parents were not home so I went to drop off my little brother at school.",
+        category: "college",
+    },
+    {
+        id: 303,
+        excuse: "PT-BR | I think due to parting last night I got suffered from food posioning.",
+        category: "college",
+    },
+    {
+        id: 304,
+        excuse: "PT-BR | My parents scolded me a lot about my behaviour towards my little brother.",
+        category: "college",
+    },
+    {
+        id: 305,
+        excuse: "PT-BR | My neighbourhood met with an accident and I was the only person who was near the accident.",
+        category: "college",
+    },
+    {
+        id: 306,
+        excuse: "PT-BR | Due to heavy rain, the roads around my area were closed.",
+        category: "college",
+    },
+    {
+        id: 307,
+        excuse: "PT-BR | A big theft happened in my house. Gotta look after my stuff.",
+        category: "college",
+    },
+    {
+        id: 308,
+        excuse: "PT-BR | My grandmother passed away today morning.",
+        category: "college",
+    },
+    {
+        id: 309,
+        excuse: "PT-BR | Me along with my parents went to a funeral of my grandmother.",
+        category: "college",
+    },
+    {
+        id: 310,
+        excuse: "PT-BR | My grandfather had a heart attack. I had to take him to the hospital.",
+        category: "college",
+    },
+
+    // ---------- Party ----------
+
+    {
+        id: 401,
+        excuse: "PT-BR | My brother can't pick up my in-laws at the airport, so I'll have to go in his place.",
+        category: "party",
+    },
+    {
+        id: 402,
+        excuse: "PT-BR | I have the worst migraine. Yeah, I don't think that I'll be able to make it to your party tonight.",
+        category: "party",
+    },
+    {
+        id: 403,
+        excuse: "PT-BR | “I went too hard at my friend's party last night. Too tired right now. I'm going to have to sit this one out.",
+        category: "party",
+    },
+    {
+        id: 404,
+        excuse: "PT-BR | My friend's parents are going to be out of town for a week. I'm going to have to take him to the hospital.",
+        category: "party",
+    },
+    {
+        id: 405,
+        excuse: "PT-BR | “I was about to drive out, but I realized quickly that I had a flat tire. My mechanic said that he won't be able to send anyone here until the morning.",
+        category: "party",
+    },
+    {
+        id: 406,
+        excuse: "PT-BR | My baby sitter canceled on me at the very last minute. Im trying to find someone else, but I highly doubt that I'll get one to come over on such short notice",
+        category: "party",
+    },
+    {
+        id: 407,
+        excuse: "PT-BR | I was on my way to the party when I got a call from my sister who had an accident. I'm sorry that I have to miss your party, but I need to be by her side right now.",
+        category: "party",
+    },
+    {
+        id: 408,
+        excuse: "PT-BR | My boss just called me saying that I need to get on a flight for an emergency meeting. I can't miss this one, but I'll send your gift to your P.O box.",
+        category: "party",
+    },
+    {
+        id: 409,
+        excuse: "PT-BR | My team just had our work schedules change, and I'm set to go in at 6 AM tomorrow.",
+        category: "party",
+    },
+    {
+        id: 410,
+        excuse: "PT-BR | That was today? I'm very sorry guys, but I promised my wife that we'd do a date night tonight.",
+        category: "party",
+    },
+    {
+        id: 411,
+        excuse: "PT-BR | I'm rushing something before midnight, it will affect my grades.",
+        category: "party",
+    },
+    {
+        id: 412,
+        excuse: "PT-BR | I was getting ready, but along the way, I picked up my phone and I'm far too cozy in this couch right now.",
+        category: "party",
+    },
+    {
+        id: 413,
+        excuse: "PT-BR | Sorry, I'm helping my friend through a really rough time now.",
+        category: "party",
+    },
+    {
+        id: 414,
+        excuse: "PT-BR | I really don't feel like seeing anyone today. Maybe next time.",
+        category: "party",
+    },
+    {
+        id: 415,
+        excuse: "PT-BR | I just got grounded by my parents. Sorry guys.",
+        category: "party",
+    },
+    {
+        id: 416,
+        excuse: "PT-BR | There will be a zombie apocalypse tomorrow, I need to rest.",
+        category: "party",
+    },
+    {
+        id: 417,
+        excuse: "PT-BR | I am my own party.",
+        category: "party",
+    },
+    {
+        id: 418,
+        excuse: "PT-BR | My ex will be there.",
+        category: "party",
+    },
+    {
+        id: 419,
+        excuse: "PT-BR | Sorry, I don't have a furry suit.",
+        category: "party",
+    },
+    {
+        id: 420,
+        excuse: "PT-BR | I really want to come but I have a screaming yeast infection and I don't want to stand around all night scratching my crotch. I really hope you understand.",
+        category: "party",
+    },
+];
--- excuseRepository.js
+++ excuseRepository.js
@@ -1,9 +1,10 @@
-var excuses = require('./Excuses');
+var excuses_eng = require('./Excuses_eng');
+var excuses_pt_br = require('./Excuses_pt_br');
 
 module.exports = {
-    getRandom: function getRandom(numberOfExcuses) {
+    getRandom: function getRandom(numberOfExcuses, language) {
+        var excuses = GetExcuseByLanguage(language);
         var limit = numberOfExcuses > excuses.length ? excuses.length : numberOfExcuses;
-
         var out = new Array(limit);
         var excuse;
 
@@ -13,21 +14,21 @@ module.exports = {
             } while (out.indexOf(excuse) > -1);
             out[i] = excuse;
         }
-        
         return out;
     },
 
-    getByID: function getByID(id) {
+    getByID: function getByID(id, language) {
+        var excuses = GetExcuseByLanguage(language);
         for (var i = 0; i < excuses.length; i++) {
             if (excuses[i].id == id) {
                 return excuses[i];
             }
         }
         return null;
-    },    
+    },
 
-    getByCategory: function getByCategory(category, numberOfExcuses) {
-
+    getByCategory: function getByCategory(category, numberOfExcuses, language) {
+        var excuses = GetExcuseByLanguage(language);
         var categoryExcuses = excuses.filter(function (excuse) {
             return excuse.category === category;
         });
@@ -43,8 +44,13 @@ module.exports = {
             } while (out.indexOf(excuse) > -1);
             out[i] = excuse;
         }
-        
         return out;
     }
-        
+}
+
+function GetExcuseByLanguage(language) {
+    if (language && language === 'pt-br')
+        return excuses_pt_br;
+
+    return excuses_eng;
 }
